3c851a4d4d8d0bb068c8e0b1580b303af79bc458
Charvii16 <149879011+Charvii16@users.noreply.github.com>
2023-11-13 15:49:27 +0530

    Python Converting Type Program
    This is built to convert int value to float and complex, float value to int and to identify the type after converting.

---
+++ convertingType.py
@@ -0,0 +1,16 @@
+#convert int to float
+x = float(1)
+
+#convert float to int
+y = int(5.3)
+
+#convert int ti complex
+z = complex(7)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
