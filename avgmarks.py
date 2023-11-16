c3e1a89912b518e46e2e81ca7b75b0709c87d0d8
Charvii16 <149879011+Charvii16@users.noreply.github.com>
2023-11-16 20:55:20 +0530

    Python Calculating Avg Marks  program
    This is built to Calculate avg marks of a student of about 4 subjects.

---
+++ avgmarks.py
@@ -0,0 +1,6 @@
+Sci = int(input("Enter Sci marks :"))
+Eng = int(input("Enter Eng marks :"))
+Maths = int(input("Enter Maths marks :"))
+Ai = int(input("Enter Ai marks :"))
+avg_marks = (Sci+Eng+Maths+Ai)/4
+print(avg_marks)
