140c70e87e6aaba2a463e30e728ab075e595cfc4
Charvii16 <149879011+Charvii16@users.noreply.github.com>
2023-11-18 20:54:49 +0530

    Python largest number program
    This is built to determine the largest number among the given three from the user.

---
+++ largest_num.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter 1st number:"))
+num2 = float(input("Enter 2nd number:"))
+num3 = float(input("Enter 3rd number:"))
+if(num1>num2)and(num1>num3):
+	print("Number 1 is largest =",num1)
+elif(num2>num1)and(num2>num3)':
+	print("Number 2 is largest=",num2)
+elif(num3>num1)and(num3>num2):
+	print("Number 3 is largest=",num3)
+else:
+	print("All numbers are same")'
