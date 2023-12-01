620983637b10c84e1ea1859d5ddbee5b394f9e9a
Charvii16 <149879011+Charvii16@users.noreply.github.com>
2023-12-01 20:36:05 +0530

    Python greeting program
    This is built to greet related to time.

---
+++ greetings_by_time.py
@@ -0,0 +1,11 @@
+t = int(input("What is the time?"))
+if t < 10:
+	print("Good Morning")
+elif t < 12:
+	print("Soon it will be time for lunch")
+elif t < 5:
+	print("Good day")
+elif t < 7:
+	print("Good evening")
+else :
+	print("Good night")
