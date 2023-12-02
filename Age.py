c740dcf1788258f1c12f96de64f24c5ff378eefc
Charvii16 <149879011+Charvii16@users.noreply.github.com>
2023-12-02 23:23:17 +0530

    Python Age Program
    This is built to find age of a person.

---
+++ Age.py
@@ -0,0 +1,14 @@
+def FunAge(x,y):
+	print(x-y)
+
+print("Charu's Age")
+FunAge(2023,2008)
+
+print("Pratick's Age")
+FunAge(2023,2000)
+
+print("Mummy's Age")
+FunAge(2023,1977)
+
+print("Papa's Age")
+FunAge(2023,1978)
